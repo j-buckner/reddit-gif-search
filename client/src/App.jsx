a896017fbcf97aec73833304e3dfc9947326845d
Jared Buckner <buckner.jared@gmail.com>
2017-02-19 16:57:09 -0600

    bleh2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ const io = require('socket.io-client');
 const socket = io.connect('/');
 
 var ReactGridLayout = require('react-grid-layout');
+var count = 0;
 
 class App extends Component {
   constructor(props) {
@@ -49,7 +50,7 @@ class App extends Component {
       if(!c_ids.includes(link.c_id)) { 
         c_ids.push(link.c_id);
         links.push(link);
-        layout.push({i: link.c_id, x: 1, y: 0, w: 350, h: 9, static: false});      
+        layout.push({i: link.c_id, x: 1, y: 0, w: 350, h: 9, static: true});      
       }
     });
     
@@ -110,6 +111,15 @@ class App extends Component {
 
     this.setState({links: linksUpdated, layout: layoutUpdated, c_ids: c_idsUpdated});
   }
+  
+  updateLayout(layout) {
+    layout.forEach(function(layoutRow, index) {
+      if (index === 0) {layoutRow.x = 0; return;}
+      let prevLayoutRow = layout[index - 1];
+      layoutRow.x = (prevLayoutRow.x + prevLayoutRow.w + layoutRow.w) <= window.innerWidth ? (prevLayoutRow.x + prevLayoutRow.w) : 0;
+    });
+    this.setState({layout: layout});
+  }
 
   onImgLoad(event) {
     const { layout } = this.state;
@@ -119,12 +129,16 @@ class App extends Component {
       return layoutRow.i === c_id;
     })
 
-    // need to figure this part out: 
-    // if (layoutRowIndex !== 0 && layout[layoutRowIndex - 1].x === 1) {
-    //   console.log('happened');
-    //   return;
-    // }
-    
+
+    let newIndex = layout.findIndex(function(layoutRow) { return (layoutRow.x !== 1); });
+    if (newIndex === -1 || newIndex === 0) newIndex = 1;
+    newIndex--;
+    // console.log('new index 1 ', newIndex);
+    //swap
+    let prevRow = layout[layoutRowIndex];
+    layout[layoutRowIndex] = layout[newIndex];
+    layout[newIndex] = prevRow;
+
     let imgWidth = 0;
     let imgHeight = 0;
     if (event.target.videoWidth && event.target.videoHeight) {
@@ -145,15 +159,16 @@ class App extends Component {
 
     // Now that the image loaded, update this link's layout row to have correct width and xPos value
     let x = 0;
-    if (layoutRowIndex !== 0) {
-      let prevLayoutRow = layout[layoutRowIndex - 1];
-      let prevWidth = prevLayoutRow.w;
-      let prevX = prevLayoutRow.x;
+    // console.log('new index: ', newIndex, 'layout is: ', layout);
+    // if (newIndex !== 0) {
+    //   let prevLayoutRow = layout[newIndex - 1];
+    //   let prevWidth = prevLayoutRow.w;
+    //   let prevX = prevLayoutRow.x;
       
-      x = (prevX + prevWidth + targetWidth) <= window.innerWidth ? (prevX + prevWidth) : 0;
-    } 
+    //   x = (prevX + prevWidth + targetWidth) <= window.innerWidth ? (prevX + prevWidth) : 0;
+    // } 
     
-    layout[layoutRowIndex] = {i: c_id, x: x, y: 0, w: targetWidth, h: 9, static: false};
+    layout[newIndex] = {i: c_id, x: x, y: 0, w: targetWidth + 12, h: 9, static: false};
 
     if (event.target.videoWidth && event.target.videoHeight) {
       event.target.setAttribute('width', targetWidth);
@@ -164,17 +179,20 @@ class App extends Component {
     }
 
     event.target.style.display = '';
+
+    count++;
+    if (count === layout.length) this.updateLayout(layout);
   }
 
   componentDidMount() {
     // turn off pagination for now
 
-    // document.addEventListener("scroll", function (event) {
-    //   if (this.getDocHeight() === this.getScrollXY()[1] + window.innerHeight) { 
-    //     this.search(this.state.currSearchText);
-    //     this.setState({"nextAfter": this.state.after});
-    //   }
-    // }.bind(this));
+    document.addEventListener("scroll", function (event) {
+      if (this.getDocHeight() === this.getScrollXY()[1] + window.innerHeight) { 
+        this.search(this.state.currSearchText);
+        this.setState({"nextAfter": this.state.after});
+      }
+    }.bind(this));
 
     socket.on('search-response', this.handleSearchResponse);
     socket.on('search-after', this.handleSearchResponseAfter);
@@ -185,6 +203,7 @@ class App extends Component {
 
   render() {
     const { links, layout } = this.state;
+    // console.log('on render: ', links, layout);
     const imgStyle = {
       'pointerEvents': 'none',
       'display': 'none'
@@ -231,3 +250,11 @@ class App extends Component {
 }
 
 export default App;
+
+// .link-div {
+//   outline: #333 solid 2px;
+// }
+
+// .link-div:hover {
+//   outline: #4747d1 solid 4px;
+// }
